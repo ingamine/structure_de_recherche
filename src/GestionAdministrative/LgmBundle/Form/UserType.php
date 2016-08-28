69a7e9e85e9ed56e57183c8c45dfd95dda6973e9
dreamamine <boubaker.am@gmail.com>
2016-08-28 22:57:28 +0100

    add template

--- src/GestionAdministrative/LgmBundle/Form/UserType.php
+++ src/GestionAdministrative/LgmBundle/Form/UserType.php
@@ -6,6 +6,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class UserType extends AbstractType
 {
@@ -16,20 +17,35 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
+            ->add('name', 'text', array(
+                'label' => 'Prénom',
+                'required' => true,
+                'constraints' => array(
+                    new Assert\NotBlank(array(
+                        'message' => 'form.error.notBlank',
+                    )),
+                    new Assert\Length(array(
+                        'min' => 2,
+                        'minMessage' => 'min deux caractères',
+                        'max' => 254,
+                        'maxMessage' => 'form.error.length.maxMessage',
+                    ))
+                )
+            ))
             ->add('lastName')
-            ->add('gender')
-            ->add('birthday', 'datetime')
+            ->add('gender', 'choice', array('choices' => array('F'=>'Féminin','M'=>'Masculin')))
+            ->add('birthday', 'birthday')
             ->add('birthPlace')
-            ->add('email')
-            ->add('phoneNumber')
+            ->add('email', 'email')
+            ->add('phoneNumber', 'number')
             ->add('grade')
             ->add('profile')
             ->add('documentId')
             ->add('photoId')
-            ->add('directeur')
-            ->add('coDirecteur')
-            ->add('effectue')
+            ->add('directeur', 'checkbox' , array('required' =>false))
+            ->add('coDirecteur', 'checkbox' , array('required' =>false))
+            ->add('effectue', 'checkbox' , array('required' =>false))
+            
             ->add('mediaId')
             
             ->add('habilitation')
