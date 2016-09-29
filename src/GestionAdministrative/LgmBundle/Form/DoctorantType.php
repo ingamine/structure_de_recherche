1f8962821f20877c89992477bf956a3c47741e45
iMech <iMech@user-PC>
2016-09-29 15:25:14 +0100

    add doctorant And CRUD

---
+++ src/GestionAdministrative/LgmBundle/Form/DoctorantType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace GestionAdministrative\LgmBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DoctorantType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('grade')
+            ->add('cINDoctorant')
+            ->add('nom')
+            ->add('prenom')
+            ->add('nomJeuneFille')
+            ->add('dateNaiss', 'date')
+            ->add('lieuNaiss')
+            ->add('sexe')
+            ->add('telMob')
+            ->add('telFixe')
+            ->add('eMail')
+            ->add('dernierDepObtenu')
+            ->add('dateDepObtenu', 'date')
+            ->add('etabDepObtenu')
+            ->add('codeStructure')
+            ->add('intituleSujet')
+            ->add('tauxAvancement')
+            ->add('anneePremierInscrip', 'date')
+            ->add('etbInscrip')
+            ->add('etabInscrip2')
+            ->add('cinEncad')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'GestionAdministrative\LgmBundle\Entity\Doctorant'
+        ));
+    }
+}
